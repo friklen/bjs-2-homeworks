02ed52349ee83efd53a5c93c0d67c9724ee82645
friklen <friklen@gmail.com>
2024-11-03 20:33:15 +0400

    initian commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [
+      (-b + Math.sqrt(discriminant)) / (2 * a),
+      (-b - Math.sqrt(discriminant)) / (2 * a),
+    ];
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+  let paymentMonth = S * (P + P / ((1 + P) ** countMonths - 1));
+  let paymentAll = +(paymentMonth * countMonths).toFixed(2);
+  return paymentAll;
 }
